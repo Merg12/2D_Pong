809e19d5dccf7a21921cf3a4e7c9a78ec2da9ded
ninetynineknights <morgan@morgans-mbp.lan>
2020-01-07 01:36:51 -0600

    separated all the functions from the void Start()

--- Assets/BallControl.cs
+++ Assets/BallControl.cs
@@ -25,28 +25,28 @@ public class BallControl : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
         Invoke("GoBall",2);
+    }
 
-        void ResetBall()
-        {
-            rb2d.velocity = new Vector2(0,0);
-            transform.position = Vector2.zero;
-        }
+    void ResetBall()
+    {
+        rb2d.velocity = new Vector2(0,0);
+        transform.position = Vector2.zero;
+    }
 
-        void RestartGame()
-        {
-            ResetBall();
-            Invoke("GoBall",1);
-        }
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("GoBall",1);
+    }
 
-        void OnCollisionEnter2D(Collision2D coll)
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.collider.CompareTag("Player"))
         {
-            if(coll.collider.CompareTag("Player"))
-            {
-                Vector2 vel;
-                vel.x = rb2d.velocity.x;
-                vel.y = (rb2d.velocity.y / 2) + (coll.collider.attachedRigidbody.velocity.y / 3);
-                rb2d.velocity = vel;
-            }
+            Vector2 vel;
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
         }
     }
 
